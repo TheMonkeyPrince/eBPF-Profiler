7bd1d7134733c8b57ea2713bd4ce531977e831c3
Clément <kyloren3600@gmail.com>
2026-03-29 20:31:54 +0200

    new scripts

---
+++ setup-rootfs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+IMG=rootfs.img
+SIZE=4G
+DIR=rootfs
+MIRROR=http://deb.debian.org/debian
+RELEASE=stable
+
+# recreate image
+rm -f "$IMG"
+qemu-img create "$IMG" "$SIZE"
+
+# format filesystem
+mkfs.ext4 "$IMG"
+
+# # unmount
+sudo umount "$DIR" || true
+# rm -rf "$DIR"
+
+# mount
+mkdir -p "$DIR"
+sudo mount -o loop "$IMG" "$DIR"
+
+# install minimal Debian
+sudo debootstrap --arch=amd64 "$RELEASE" "$DIR" "$MIRROR"
+
+# unmount
+sleep 2 # ensure all writes are flushed
+sudo umount -l "$DIR"
+rm -rf "$DIR"
