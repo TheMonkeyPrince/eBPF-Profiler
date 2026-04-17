6183e5ead3030f6644a5f9a027454c18abe19240
Clément <kyloren3600@gmail.com>
2026-04-17 11:13:33 +0200

    track start and end times

--- kernel_patch/bpf/profiler.h
+++ kernel_patch/bpf/profiler.h
@@ -4,32 +4,30 @@
 
 #define BPF_PROFILER_NO_ARG ((u32)-1)
 
-#define RUN_BLOCK_WITH_BPF_TIMER_AND_ARG(arg, code_block)                     \
-    do {                                                         \
-        u64 __bpf_timer_start = ktime_get_ns();                              \
-        code_block                                               \
-        u64 __bpf_timer_end = ktime_get_ns();                                \
-        u64 duration = __bpf_timer_end - __bpf_timer_start;                              \
-        bpf_profiler_block_timer_result(__FILE__, __LINE__, duration, arg); \
+#define RUN_BLOCK_WITH_BPF_TIMER_AND_ARG(arg, code_block)                                             \
+    do {                                                                                              \
+        u64 __bpf_timer_start = ktime_get_ns();                                                       \
+        code_block                                                                                    \
+        u64 __bpf_timer_end = ktime_get_ns();                                                         \
+        bpf_profiler_block_timer_result(__FILE__, __LINE__, __bpf_timer_start, __bpf_timer_end, arg); \
     } while (0)
 
 #define RUN_BLOCK_WITH_BPF_TIMER(code_block)                     \
     RUN_BLOCK_WITH_BPF_TIMER_AND_ARG(BPF_PROFILER_NO_ARG, code_block)
 
-#define CALL_WITH_BPF_TIMER_AND_ARG(arg, func, ...)                                     \
-({                                                                         \
-    u64 __bpf_timer_start = ktime_get_ns();                                          \
-    __auto_type __ret_val = func(__VA_ARGS__);                             \
-    u64 __bpf_timer_end = ktime_get_ns();                                            \
-    u64 __duration = __bpf_timer_end - __bpf_timer_start;                                      \
-    bpf_profiler_func_timer_result(__FILE__, __LINE__, __duration, arg, #func); \
-    __ret_val;                                                             \
+#define CALL_WITH_BPF_TIMER_AND_ARG(arg, func, ...)                                                     \
+({                                                                                                      \
+    u64 __bpf_timer_start = ktime_get_ns();                                                             \
+    __auto_type __ret_val = func(__VA_ARGS__);                                                          \
+    u64 __bpf_timer_end = ktime_get_ns();                                                               \
+    bpf_profiler_func_timer_result(__FILE__, __LINE__, __bpf_timer_start, __bpf_timer_end, arg, #func); \
+    __ret_val;                                                                                          \
 })
 
 #define CALL_WITH_BPF_TIMER(func, ...) \
     CALL_WITH_BPF_TIMER_AND_ARG(BPF_PROFILER_NO_ARG, func, __VA_ARGS__)
 
-void bpf_profiler_block_timer_result(const char* file, const int start_line, const u64 duration, u32 arg);
-void bpf_profiler_func_timer_result(const char* file, const int line, const u64 duration, u32 arg, const char* func_name);
+void bpf_profiler_block_timer_result(const char* file, const int start_line, const u64 start_time, const u64 end_time, u32 arg);
+void bpf_profiler_func_timer_result(const char* file, const int line, const u64 start_time, const u64 end_time, u32 arg, const char* func_name);
 
 #endif
